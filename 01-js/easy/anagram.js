899e343c0091dba2fd2fa73ad8b67652b3c99696
Prabesh <tandukarprabesh@gmail.com>
2024-02-05 02:18:24 +1300

    u

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,6 +5,33 @@
 */
 ///test
 
-function isAnagram(str1, str2) {}
+function isAnagram(str1, str2) {
+  let similarityCount = 0;
+  let word = "";
+  if (str1.length == str2.length) {
+    for (let i = 0; i < str1.length; i++) {
+      for (let j = 0; j < str2.length; j++) {
+        // console.log(str1[i], str2[j]);
+        if (str1[i] == str2[j]) {
+          // similarityCount++;
+          word += str2[j];
+        }
+      }
+    }
+    if (str1 == word) {
+      console.log(word);
+      return true;
+    } else {
+      console.log(word);
+      return false;
+    }
+  } else {
+    console.log(word);
+    return false;
+  }
+}
 
 module.exports = isAnagram;
+
+const ans = isAnagram("carccc", "racccc");
+console.log(ans);
